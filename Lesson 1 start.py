afb1602d454ebbb4901f4fb9c95c19a1cbc574a8
zultone <87732087+zultone@users.noreply.github.com>
2021-08-31 21:50:24 -0400

    Add files via upload

---
+++ Lesson 1 start.py
@@ -0,0 +1,68 @@
+# This indicates a comment
+
+one = 1 # Interger variable: This is a whole number.
+        # Python can do math with this
+
+decimal = 1.1 # This is called a "float"
+              # Python uses a floating decimal system for math
+              
+stringVar = "Hello World" # This is a string "A string of text"
+
+trueOrFalse = True # Boolean: It is either True, or False.
+
+
+
+one = 1
+two = 2
+three = 3
+print(one)
+print(two)
+print(three)
+two = 4
+print(two)
+print(one)
+
+Decimal = 1.1
+print(Decimal)
+
+StringVar = "Hello World"
+print(StringVar)
+
+# Left what you're giving the value to
+# Right what the value is
+
+
+
+##5 = Five
+##print(Five) This will induce an error
+hWorld = "Hello "
+def FunctionName():
+    newVar = "World"
+    print(one)
+    print(hWorld + newVar)
+    return
+#Call function for example
+#Functions won't "do" anything until they are called
+#Variables inside a function do not exist outside the function
+FunctionName()
+
+# print(newVar) # This variable exists only in "FunctionName()"
+
+count = 0
+print(count) 
+count = count + 1 # We add things to variables and with variables
+print(count)
+count += 1 # This is the same thing as "count = count + 1"
+print(count)
+
+count = 0 
+for i in range(5): # This is an example of a "for" loop.
+    count +=1
+    print(count)
+
+
+
+
+
+
+
